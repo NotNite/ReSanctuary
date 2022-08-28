bd4e32f395bc194d01bf059625c9b38b69631be6
NotNite <hi@notnite.com>
2022-08-27 21:12:43 -0400

    1.1.0.7

--- ReSanctuary/Windows/WidgetWindow.cs
+++ ReSanctuary/Windows/WidgetWindow.cs
@@ -15,12 +15,12 @@ namespace ReSanctuary.Windows;
 
 public class WidgetWindow : Window, IDisposable {
     private Plugin Plugin;
-    
+
     // am i lazy for having these in two places? yes
     // do i care? no
     private RawExcelSheet itemPouchSheet;
     private ExcelSheet<Item> itemSheet;
-    
+
     private Dictionary<uint, TextureWrap> todoTextureCache;
 
     public WidgetWindow(Plugin plugin) : base("ReSanctuary Widget") {
@@ -28,12 +28,12 @@ public class WidgetWindow : Window, IDisposable {
             MinimumSize = new Vector2(150, 75) * ImGuiHelpers.GlobalScale,
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
-   
+
         Plugin = plugin;
-        
+
         itemPouchSheet = Plugin.DataManager.Excel.GetSheetRaw("MJIItemPouch");
         itemSheet = Plugin.DataManager.Excel.GetSheet<Item>();
-        
+
         todoTextureCache = new();
     }
 
@@ -41,27 +41,30 @@ public class WidgetWindow : Window, IDisposable {
 
     public override void PreDraw() {
         if (Plugin.Configuration.LockWidget) {
-            Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoDocking | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoTitleBar;
+            Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoDocking | ImGuiWindowFlags.NoMove |
+                    ImGuiWindowFlags.NoTitleBar;
+            RespectCloseHotkey = false;
         } else {
             Flags = ImGuiWindowFlags.None;
+            RespectCloseHotkey = true;
         }
     }
 
     private void DrawGarbage(uint id, int amount, Item item, int has) {
         TextureWrap? icon;
         if (todoTextureCache.ContainsKey(id)) {
-            icon = todoTextureCache[id]; 
+            icon = todoTextureCache[id];
         } else {
             icon = Plugin.DataManager.GetImGuiTextureIcon(item.Icon);
             todoTextureCache[id] = icon;
         }
-            
+
         var iconSize = ImGui.GetTextLineHeight() * 1.25f;
         var iconSizeVec = new Vector2(iconSize, iconSize);
         ImGui.Image(icon.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
 
         ImGui.SameLine();
-        
+
         if (has >= amount) ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.HealerGreen);
         if (ImGui.Selectable($"{item.Name} - {has}/{amount}")) {
             var mat = Utils.GetSortedGatheringItems().Find(x => x.ItemID == item.RowId);
@@ -73,27 +76,27 @@ public class WidgetWindow : Window, IDisposable {
                 Utils.OpenGatheringMarker(teri, mat.X, mat.Y, mat.Radius, mat.Name);
             }
         }
-        if (has >= amount) ImGui.PopStyleColor();
 
+        if (has >= amount) ImGui.PopStyleColor();
     }
 
     public override void Draw() {
         var todoList = Plugin.Configuration.TodoList;
-        
+
         // i just got home from class and i am exhausted. here is some very bad unoptimized code\
         foreach (var (id, amount) in todoList) {
             var itemPouchItem = itemPouchSheet.GetRow(id);
             var item = itemSheet.GetRow(itemPouchItem.ReadColumn<uint>(0));
             var has = Utils.GetStackSize(item.RowId);
-            
+
             if (has < amount) DrawGarbage(id, amount, item, has);
         }
-        
+
         foreach (var (id, amount) in todoList) {
             var itemPouchItem = itemPouchSheet.GetRow(id);
             var item = itemSheet.GetRow(itemPouchItem.ReadColumn<uint>(0));
             var has = Utils.GetStackSize(item.RowId);
-            
+
             if (has >= amount) DrawGarbage(id, amount, item, has);
         }
     }
