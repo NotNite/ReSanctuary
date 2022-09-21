21d7c5506aea9091af75b8f228716e914054a85c
DaCyclops <abcyclops@optusnet.com.au>
2022-09-21 17:52:46 +1000

    Base config options to support Creature tab

--- ReSanctuary/Configuration.cs
+++ ReSanctuary/Configuration.cs
@@ -10,7 +10,10 @@ public class Configuration : IPluginConfiguration {
     public int Version { get; set; } = 1;
 
     public Dictionary<uint, int> TodoList { get; set; } = new();
+    public Dictionary<uint, DateTime> TodoListCreature { get; set; } = new();
+    public List<uint> CreatureFilterHide { get; set; } = new();
     public bool LockWidget { get; set; }
+    
 
     // the below exist just to make saving less cumbersome
     [NonSerialized] private DalamudPluginInterface? PluginInterface;
